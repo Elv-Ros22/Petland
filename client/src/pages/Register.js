840dfe8e5007402817f496387f0da49746d1d627
Elv-Ros22 <elviamanrique00@gmail.com>
2025-06-08 18:30:25 -0500

    update register file

---
+++ client/src/pages/Register.js
@@ -0,0 +1,47 @@
+import { useState } from "react";
+import axios from "axios";
+import { useNavigate, Link } from "react-router-dom";
+
+export default function Register() {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [message, setMessage] = useState("");
+  const navigate = useNavigate();
+
+  const handleRegister = async (e) => {
+    e.preventDafault();
+    try {
+      await axios.post("http://localhost:5000/api/users/register", {
+        username,
+        password,
+      });
+      navigate("/");
+    } catch (error) {
+      setMessage(error.response.data.message || "Registration failed");
+    }
+  };
+
+  return (
+    <div>
+      <h2>Register</h2>
+      <form onSubmit={handleRegister}>
+        <input
+          value={username}
+          onChange={(e) => setUsername(e.target.value)}
+          placeholder="Username"
+          required
+        />
+        <input
+          type="password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+          placeholder="Password"
+          required
+        />
+        <button>Register</button>
+      </form>
+      <p>{message}</p>
+      <Link to="/">Back to Login</Link>
+    </div>
+  );
+}
