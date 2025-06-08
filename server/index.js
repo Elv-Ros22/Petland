969b8d9325e76980f5a1cc1d6cd92a9cbf35396c
Elv-Ros22 <elviamanrique00@gmail.com>
2025-06-08 16:22:16 -0500

    update file

--- server/index.js
+++ server/index.js
@@ -8,7 +8,7 @@ app.use(cors());
 app.use(express.json());
 
 mongoose
-  .connect(process.env.MONGODB_URI, {
+  .connect(process.env.MONGO_URI, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
   })
