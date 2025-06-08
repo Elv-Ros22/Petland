4501a0521fba9f48ca0ac04c26994de656953270
Elv-Ros22 <elviamanrique00@gmail.com>
2025-06-08 17:07:28 -0500

    added userRoutes file

---
+++ server/routes/userRoutes.js
@@ -0,0 +1,28 @@
+const express = require("express");
+const router = express.Router();
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+const User = require("../models/User");
+
+router.post("/register", async (req, res) => {
+  const { username, password } = req.body;
+  const existing = await User.findOne({ username });
+  if (existing) return res.status(400).json({ msg: "User already exists" });
+
+  const hashed = await bcrypt.hash(password, 10);
+  const newUser = new User({ username, password: hashed });
+  await newUser.save();
+  res.json({ msg: "User registered" });
+});
+
+router.post("/login", async (req, res) => {
+  const { username, password } = req.body;
+  const user = await User.findOne({ username });
+  if (!user || !(await bcrypt.compare(password, user.password)))
+    return res.status(401).json({ msg: "Invalid credentials" });
+
+  const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET);
+  res.json({ token, username: user.username });
+});
+
+module.exports = router;
