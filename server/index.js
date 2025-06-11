3ca147a441e0712d35ab5f44642f7a7bf19ccd37
Elv-Ros22 <elviamanrique00@gmail.com>
2025-06-10 21:38:49 -0500

    updated index.js file

--- server/index.js
+++ server/index.js
@@ -8,18 +8,12 @@ app.use(cors());
 app.use(express.json());
 
 mongoose
-  .connect(process.env.MONGO_URI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  })
+  .connect(process.env.MONGO_URI)
   .then(() => console.log("MongoDB connected"))
   .catch((err) => console.error(err));
 
-// Routes
 app.use("/api/users", require("./routes/userRoutes"));
 app.use("/api/questions", require("./routes/questionRoutes"));
 
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
-});
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
